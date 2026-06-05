c5885b36b85e4e8d87dac985da87ab36b0b2bc7a
Shrey Dhola <shreydhola.007@gmail.com>
2026-06-05 09:40:22 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,63 @@
 import os
+import sys
+
+import numpy as np 
+import pandas as pd
+import dill
 import pickle
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
+from src.exception import CustomException
 
-def save_object(file_path: str, obj) -> None:
-    """Serialize `obj` to `file_path` using pickle."""
-    dir_name = os.path.dirname(file_path)
-    if dir_name:
-        os.makedirs(dir_name, exist_ok=True)
-    with open(file_path, "wb") as f:
-        pickle.dump(obj, f)
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
 
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
+
+            try:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+
+                model.set_params(**gs.best_params_)
+                model.fit(X_train, y_train)
+
+                y_test_pred = model.predict(X_test)
+                test_model_score = r2_score(y_test, y_test_pred)
+
+                report[model_name] = test_model_score
+            except Exception as e:
+                # Skip failing models so training can still proceed and save a usable model
+                continue
+
+        if not report:
+            raise CustomException("No models were successfully trained.", sys)
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
